39aa193b21e2ffe83586b6644d621d077dc99b98
DarioCastel <lecastelnovo@gmail.com>
2024-01-12 19:36:06 -0300

    descriptionV1

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -1,11 +1,34 @@
-import "../css/galery.css"
+import "../css/galery.css";
+import Thumb1 from "../img/image-product-1-thumbnail.jpg"
+import Thumb2 from "../img/image-product-2-thumbnail.jpg"
+import Thumb3 from "../img/image-product-3-thumbnail.jpg"
+import Thumb4 from "../img/image-product-4-thumbnail.jpg"
+import Product1 from "../img/image-product-1.jpg"
 
 const Galery = () => {
   return (
     <>
-    <div className="galeryContainer">galeria</div>
+      <div className="galeryContainer">
+        <div className="previewMain">
+          <img src={Product1} alt="" />
+        </div>
+        <div className="smallPreviewContainer">
+          <span className="previewPhoto1">
+            <img src={Thumb1} alt="" />
+          </span>
+          <span className="previewPhoto2">
+          <img src={Thumb2} alt="" />
+          </span>
+          <span className="previewPhoto3">
+          <img src={Thumb3} alt="" />
+          </span>
+          <span className="previewPhoto4">
+          <img src={Thumb4} alt="" />
+          </span>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Galery
+export default Galery;
--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,13 +1,36 @@
-import "../css/productInfo.css"
+import "../css/productInfo.css";
 
 const ProductInfo = () => {
   return (
     <>
-    <div className="productInfoContainer">
-        info de producto
-    </div>
+      <div className="productInfoContainer">
+        <span className="titleCompany">SNEAKER COMPANY</span>
+        <h1>Fall Limited Edition Sneakers</h1>
+        <span className="descriptionProduct">
+          <p>
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, theyll withstand everything
+            the weather can offer.
+          </p>
+        </span>
+        <div className="priceContainer">
+          <div className="priceCalculated">
+            <span className="actualPrice">$125.00</span>
+            <span className="discountPrice">%50</span>
+          </div>
+          <span className="priceList">$250.00</span>
+        </div>
+        <div className="cantCartContainer">
+          <div className="itemUnits">
+            <button className="minusButton">-</button>
+            <span className="quantityProduct">2</span>
+            <button className="plusButton">+</button>
+          </div>
+          <button className="addButton">Add to cart</button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ProductInfo
+export default ProductInfo;
