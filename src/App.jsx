962549489904360597e872468739727e040bdc8e
DarioCastel <lecastelnovo@gmail.com>
2024-01-15 10:51:35 -0300

    mainBodyV1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import BodyMain from "./components/BodyMain"
 import HeadMain from "./components/HeadMain"
+import "./app.css"
 
 function App() {
   return (
     <>
+    <main>
       <HeadMain/>
       <BodyMain/>
+    </main>
     </>
   )
 }
--- src/components/HeadMain.jsx
+++ src/components/HeadMain.jsx
@@ -1,12 +1,16 @@
 import "../css/headMain.css";
 import ImgAvatar from "../img/image-avatar.png";
+import Logo from "../img/logo.svg"
+import Cart from "../img/icon-cart.svg"
 
 const headMain = () => {
   return (
     <>
       <div className="headContainer">
         <div className="headElementsLeft">
-          <div className="headtitle">sneakers</div>
+          <div className="headTitle">
+            <img src={Logo} alt="" />
+          </div>
           <div className="headButtons">
             <span>Collections</span>
             <span>Men</span>
@@ -16,7 +20,10 @@ const headMain = () => {
           </div>
         </div>
         <div className="headElementsRight">
-          <div className="shopCart">carringui</div>
+          <div className="shopCart">
+            <img src={Cart} alt="" />
+            <span className="contCart">0</span>
+          </div>
           <div className="headAvatar">
             <img src={ImgAvatar} alt="" />
           </div>
